70c8a602e969f44d2d951003dbfe002d1b266a70
Ermelo1983 <ermelo1983@gmail.com>
2018-04-16 10:07:55 +0200

    changed method name to right name

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,14 +6,14 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates_presence_of :user_id
-  validate :body_xor_image_url
+  validate :body_x_and_or_image_url
   validates :image_url, :file_size => { :maximum => 5.megabytes.to_i  }
 
 
-  def body_xor_image_url
+  def body_x_and_or_image_url
     unless self.body.present? && self.image_url.present?
-      errors.add :body, :body_xor_image_url
-      errors.add :image_url, :body_xor_image_url
+      errors.add :body, :body_x_and_or_image_url
+      errors.add :image_url, :body_x_and_or_image_url
     end
   end
 end
