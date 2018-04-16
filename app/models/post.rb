2584e2703a3810394e374ee1f732f0dc44c00421
Ermelo1983 <ermelo1983@gmail.com>
2018-04-16 09:41:00 +0200

    changed the XOR validation, made mistake to not accept both, and added file size validation for uploading on post also have to add it to profile and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+require 'file_size_validator'
+
 class Post < ApplicationRecord
   mount_uploader :image_url, ImageUploader
   belongs_to :user
@@ -5,10 +7,11 @@ class Post < ApplicationRecord
 
   validates_presence_of :user_id
   validate :body_xor_image_url
+  validates :image_url, :file_size => { :maximum => 5.megabytes.to_i  }
 
 
   def body_xor_image_url
-    unless self.body.present? ^ self.image_url.present?
+    unless self.body.present? && self.image_url.present?
       errors.add :body, :body_xor_image_url
       errors.add :image_url, :body_xor_image_url
     end
